2dc8de78bd8c4afc65d5d82dfbb90690e128c519
mastresalomas <mastersalomas@gmail.com>
2026-08-13 15:58:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * >>> AO PUBLICAR UMA NOVA VERSÃO, ATUALIZE A LINHA ABAIXO. <<<
  * Sem isso o navegador não descarta o pacote anterior.
  */
-const VERSAO = 'v21';
+const VERSAO = 'v22;
 
 const CACHE = 'recomp-' + VERSAO;
 
